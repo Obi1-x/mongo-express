0ca8e892bf10caf975916dde9debc35eec86747c
Okezie <chekwube102@gmail.com>
2023-04-06 01:18:37 +0100

    Building Express. Removed router

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const express = require('express'); //returns a function
 const app = express(); //Returns an object of type Express
 app.use(express.json()); //Allows our server to accept JSON parsing as a body in POST command or so.
 
-const router = express.Router();
-app.use('/random', router);
-
-//const serverless = require('serverless-http');
-
-
 const bot_Import = require('./to_frontend/telegrafAPI');
 const botMod = bot_Import.bot;
 const bToken = bot_Import.botToken;
@@ -22,19 +16,19 @@ const repo = bot_Import.kBoards.daBase;
 //botMod.startWebhook("/" + bToken, null, null); //To start the webhook.
 
 
-router.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
     console.log("Welcome to the main endpoint!");
     res.status(200).send("Hello World, Welcome to my Lambda function endpoint.");
 });
 
-router.get('/logs', async (req, res) => {
+app.get('/logs', async (req, res) => {
     console.log("Logs endpoint!");
     res.send(repo.dbLogs);
 });
 
 
 
-router.get('/dbread', async (req, res) => {
+app.get('/dbread', async (req, res) => {
     var aUser = {
         "id": 1355312020,
         "first_name": "Drake",
@@ -49,13 +43,13 @@ router.get('/dbread', async (req, res) => {
     //res.send(`Read complete wrote`);
 });
 
-router.get('/dbconnect', async (req, res) => {
+app.get('/dbconnect', async (req, res) => {
     console.log("DB endpoint!", "Connecting to DB...");
     const response = await repo.connectDB();
     res.status(200).send(response);
 });
 
-router.get('/dbdisconnect', async (req, res) => {
+app.get('/dbdisconnect', async (req, res) => {
     console.log("DB endpoint!", "Disconnecting DB...");
     const response = await repo.disconnectDB();
     res.status(200).send(response);
@@ -67,10 +61,10 @@ router.get('/dbwrite', async (req, res) => {
 });*/
 
 
-module.exports = app;
+//module.exports = app;
 //module.exports.handler = serverless(app);
 
-/*
+
 const port = 8085;
 app.listen(port, () => console.log(`Listening at ${port}`));
-botMod.launch();*/
+botMod.launch();
